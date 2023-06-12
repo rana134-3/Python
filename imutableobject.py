e98c0451b3286103a70dc4a7ce49918b35ef6836
rana134-3 <110804571+rana134-3@users.noreply.github.com>
2023-06-12 21:22:26 +0530

    Delete imutableobject.py

--- imutableobject.py
+++
@@ -1,5 +0,0 @@
-#program to change value at a given index of a string
-s = "Hello World"
-print("string before changing value at index 8: ",s)
-s[8] = "l"
-print("list after changing value at index 8: ",s)
