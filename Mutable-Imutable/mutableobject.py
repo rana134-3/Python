f920c8726b72ea7783e76abde8a796ec039a7ae2
rana134-3 <110804571+rana134-3@users.noreply.github.com>
2023-06-12 21:12:46 +0530

    Add files via upload

---
+++ Mutable-Imutable/mutableobject.py
@@ -0,0 +1,5 @@
+#program to change value at a given index of a list
+l = [1,5,"hello",9.8,88, "everyone",55.69]
+print("list before changing value at index 5: ",l)
+l[5] = "world"
+print("list after changing value at index 5: ",l)
