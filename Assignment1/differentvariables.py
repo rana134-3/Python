e65aa2dbb02a59999dbe3c537c28cd7dfbd38b90
Ayush Rana <110804571+rana134-3@users.noreply.github.com>
2023-06-13 22:17:48 +0530

    Variables
    Different types of variables: String, List, Float, Tuple

---
+++ Assignment1/differentvariables.py
@@ -0,0 +1,13 @@
+# Variables: string, list, float, tuple 
+s= "Hello World"
+l= [1,5,True,"hello", 2.58, "world", 55]
+f= 88.65
+t= (1,5,True,"hello", 2.58, "world", 55)
+print("string: ",s)
+print (type(s))
+print("list: ",l)
+print(type(l))
+print("float: ",f)
+print(type(f))
+print("tuple: ",t)
+print(type(t))
