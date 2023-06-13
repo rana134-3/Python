80d17e4330d73559732368147464362be68728b0
Ayush Rana <110804571+rana134-3@users.noreply.github.com>
2023-06-13 22:26:40 +0530

    Immutable Object
    Example of immutable object

---
+++ Assignment1/imutableobject.py
@@ -0,0 +1,5 @@
+#Immutable object are those object whose value can not be changed after creation.
+s = "Hello World"
+print("string before changing value at index 8: ",s)
+s[8] = "l"
+print("list after changing value at index 8: ",s)
