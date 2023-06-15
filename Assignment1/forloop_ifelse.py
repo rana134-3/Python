6c998b64f9dc9a39831c0940b84a500b68d4f3e2
Ayush Rana <110804571+rana134-3@users.noreply.github.com>
2023-06-15 20:47:24 +0530

    Delete forloop_ifelse.py

--- Assignment1/forloop_ifelse.py
+++
@@ -1,7 +0,0 @@
-#create a list containin 25 int values. Using for loop and if else condition printif the element is divisible by 3 or not.
-l= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-for i in l:
-    if i%3==0:
-        print(i,"is divisible by 3")
-    else:
-        print(i,"is not divisible by 3")
