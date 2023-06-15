bb418256520df7b56676e6de1511f0e4f2df81a3
Ayush Rana <110804571+rana134-3@users.noreply.github.com>
2023-06-15 20:48:40 +0530

    Delete mutableobject.py

--- Assignment1/mutableobject.py
+++
@@ -1,5 +0,0 @@
-#Mutable objects are those objects whose value can be changed after they are created.
-l = [1,5,"hello",9.8,88, "everyone",55.69]
-print("list before changing value at index 5: ",l)
-l[5] = "world"
-print("list after changing value at index 5: ",l)
